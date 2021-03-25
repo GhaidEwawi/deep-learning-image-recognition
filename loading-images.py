ed7ed45de33600a9b270fb87b34daae2b62fa8fd
GhaidEwawi <ghaid_99@hotmail.com>
2021-03-25 14:33:56 +0200

    finishing up the model

--- loading-images.py
+++ loading-images.py
@@ -14,8 +14,6 @@ x_train /= 255
 x_test /= 255
 
 # Convert class vectors to binary class matrices
-# Our labels are single values from 0 to 9.
-# Instead, we want each label to be an array with on element set to 1 and and the rest set to 0.
 y_train = keras.utils.to_categorical(y_train, 10)
 y_test = keras.utils.to_categorical(y_test, 10)
 
@@ -23,19 +21,28 @@ y_test = keras.utils.to_categorical(y_test, 10)
 model = Sequential()
 
 # Create convolutional layers
-model.add(Conv2D(32, (3, 3), padding="same", activation="relu", input_shape=(32, 32, 3)))
+model.add(Conv2D(32, (3, 3), padding='same', input_shape=(32, 32, 3), activation="relu"))
 model.add(Conv2D(32, (3, 3), activation="relu"))
 model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
 
 model.add(Conv2D(64, (3, 3), padding='same', activation="relu"))
 model.add(Conv2D(64, (3, 3), activation="relu"))
 model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
 
 model.add(Flatten())
-
-
-model.add(Dense(512, activation="relu", input_shape=(32, 32, 3)))
+model.add(Dense(512, activation="relu"))
+model.add(Dropout(0.5))
 model.add(Dense(10, activation="softmax"))
+
+# Compile the model
+model.compile(
+    loss='categorical_crossentropy',
+    optimizer='adam',
+    metrics=['accuracy']
+)
+
 
 # Print a summary of the model
 model.summary()
